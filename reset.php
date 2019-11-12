03bc52d6227da05f7d092b7ac5b3146940bdaf39
kandersen90 <54188123+kandersen90@users.noreply.github.com>
2019-11-12 13:04:09 -0500

    added privacy link

--- reset.php
+++ reset.php
@@ -92,7 +92,7 @@
 
 	<footer class="page-footer font-small blue">
 				<div class="footer-copyright text-center">
-					<a href="about.php">About Yapt</a><br />
+					<a href="about.php">About Yapt</a> <a href="#">Privacy</a><br />
 					<font color="white"> &copy; 2018 - <?php echo date("Y"); ?> <a href="http://www.netlander.com" target="_blank" >NetLander, Inc.</a><br />
 					<small color="white">Made in Florida <i class="fal fa-rocket"></i></small>
 				</div>
