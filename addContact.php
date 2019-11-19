e8660e15cc036f226c003dc527ba5f4752e8ac0f
kandersen90 <54188123+kandersen90@users.noreply.github.com>
2019-11-19 10:41:18 -0500

    added privacy page

--- addContact.php
+++ addContact.php
@@ -118,7 +118,7 @@ gtag('config', 'UA-107709825-2');
 
 			<footer class="page-footer font-small blue bg-light">
 				<div class="footer-copyright text-center">
-					<a href="aboutYapt.php">About Yapt</a> <a href="#">Privacy</a><br />
+					<a href="aboutYapt.php">About Yapt</a> - <a href="privacy.php">Privacy</a><br />
 					<font color="black"> &copy; 2018 - <?php echo date("Y"); ?> <a href="http://www.netlander.com" target="_blank" >NetLander, Inc.</a><br />
 					<small color="black">Made in Florida <i class="fal fa-rocket"></i></small>
 				</div>
--- privacy.php
+++ privacy.php
@@ -11,26 +11,29 @@
 *************************************************************/?>
 <!DOCTYPE html>
 <html>
+	<?php include('topbar.php')?>
  <head>
+   <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+   <link href="css/Styles.css" rel="stylesheet" type="text/css">
 
+   <meta name="viewport" content="width = device-width, initial-scale = 1">
 
 <title> Privacy Policy </title>
 
 </head>
 
-
-		<?php include('topbar.php')?>
     <body>
-      <p style="text-align:left;font-size:180%;"> <strong>Yapt Online Privacy Policy</strong></p>
-        <p style="text-left;font-size:140%;"> Thank you for entrusting Yapt Online for your life productivity tool and personal information. Having access to your private inforamtion is a serious responsibility, and we want to let you know how we're handling it.</p>
-          <p style="text-left;font-size:140%"> We only collect the information you choose to give us, and we process it with your consent, we only require the minimum amount of personal information that is necessary to fulfill the purpose of your interaction with us; we don't sell it to third parties; and we only use it as this Privacy Statement describes.</p>
+			<section style="padding:8rem 0 5rem 0">
+       <p style="text-align:center;font-size:180%;"> <strong>Yapt Online Privacy Policy</strong></p>
+         <p style="text-align:center;font-size:140%;"> Thank you for entrusting Yapt Online for your life productivity tool and personal information. Having access to your private information is a serious responsibility, and we want to let you know how we're handling it.</p>
+           <p style="text-align:center;font-size:140%"> We only collect the information you choose to give us, and we process it with your consent, we only require the minimum amount of personal information that is necessary to fulfill the purpose of your interaction with us; we don't sell it to third parties; and we only use it as this Privacy Statement describes.</p>
 
 
 
 
 
 
-
+ </section>
 </body>
 
 
@@ -42,7 +45,7 @@
 <section class="bg-light">
   <footer class="page-footer font-small blue">
     <div class="footer-copyright text-center">
-      <a href="about.php">About Yapt</a> <a href="#">Privacy</a><br />
+      <a href="about.php">About Yapt</a> - <a href="#">Privacy</a><br />
       <font color="black"> &copy; 2018 - <?php echo date("Y"); ?> <a href="http://www.netlander.com" target="_blank" >NetLander, Inc.</a><br />
       <small color="black">Made in Florida <i class="fal fa-rocket"></i></small>
     </div>
